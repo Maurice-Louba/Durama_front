5caad5a9b361541dda79c148dd03e6a211861075
Maurice-Louba <mauricelouba9@gmail.com>
2025-11-20 23:19:21 +0100

    ajout pour deploiement

--- src/profil/Toutprofil.tsx
+++ src/profil/Toutprofil.tsx
@@ -1,6 +1,6 @@
 import Footer from "../HomePage/Footer/Footer"
 import MobileNavbar from "../HomePage/MobileNavbar/MobileNavbar"
-import Navbar from "../HomePage/Navbar/Navbar"
+
 import LoginForm from "./creerprofil/creerprofil"
 
 const Toutprofil = () => {
